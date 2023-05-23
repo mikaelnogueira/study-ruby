39ab846dd29c062979042f9049acd9f000b70f52
mikaelnogueira <nog.mikael@gmail.com>
2023-05-22 22:35:42 -0300

    update 012 and 013

--- basic/013.rb
+++ basic/013.rb
@@ -1,13 +1,16 @@
 #Ruby Study
-  #012
-    #Inverter linhas usando o reverse
-    #E com o Join, a linhas ficaram mais organizadaz
+  #013
+    #Formatação de texto
+    #Intruduzindo alguns metódos para formatar o texto.
 
 #CODE
 
 variavel = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-puts variavel.lines.reverse.join
+#downcase deixa todas as letras minusculas
+puts variavel.downcase
 
+puts (' ')
 
-
+#swapcase inverte as caaracteris dos caracteres
+puts variavel.swapcase
